c9c7914ac2243a137c740e3f0c273e1ba9d117a2
Jeremy Pulmano <jeremypulmano@gmail.com>
2020-07-31 01:16:14 -0400

    upgrade to 650 iter

--- server.py
+++ server.py
@@ -42,7 +42,7 @@ class Battlesnake(object):
         self.height = 23
         
         # Make our policy from previous weights
-        self.policy = make_policy(self.layers, self.width, self.height, "weights/weights-400iter-aggressive.pt")
+        self.policy = make_policy(self.layers, self.width, self.height, "weights/weights-650iter-aggressive.pt")
         self.policy.eval()
         
         # Try using symmetry
